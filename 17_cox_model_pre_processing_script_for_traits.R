5df3bc1b3ec4a484c075af4062e33c9061fbef01
DanniGadd <93707608+DanniGadd@users.noreply.github.com>
2022-02-15 16:34:57 +0000

    Update 17_cox_model_pre_processing_script_for_traits.R

--- 17_cox_model_pre_processing_script_for_traits.R
+++ 17_cox_model_pre_processing_script_for_traits.R
@@ -1,4 +1,10 @@
 
+Copyright (c) <2022>, <DanniGadd>
+All rights reserved.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory.
+
 ####################################################################################
 
 ## Read in Incidence Files for each trait and combine pirmary and secondary cases
